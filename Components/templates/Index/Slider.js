3f8de4f0e5817ac898dd7a42d9d7333d69f562c5
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-08 13:09:26 +0330

    slider style added

--- Components/templates/Index/Slider.js
+++ Components/templates/Index/Slider.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
+
 function Slider() {
   return <div>Slider</div>;
 }
